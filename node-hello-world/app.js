56697bbe791123c80321b9800bdb0efff968b12a
jiuzhou.chow <jiuzhou.zhou9@gmail.com>
2023-08-25 11:34:57 +0900

    up

--- node-hello-world/app.js
+++ node-hello-world/app.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const bodyParser = require('body-parser'); // 引入 body-parser
+const app = express();
+const port = 8080;
+
+app.use(bodyParser.json()); // 使用 body-parser 中间件来解析 JSON 请求体
 
 app.get('/', (req, res) => {
-  res.send('Hello Express.js!!!')
-})
+  res.send('Hello Express.js!!!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get('/user', (req, res) => {
+  res.send('Hello path/user !!!');
+});
+
+app.post("/create", function (req, res) {
+  console.log(1111);
+  res.send(req.body.msg);
+});
