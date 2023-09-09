34a9d317441893f992b2ebeca9bb81d11c09a416
jiuzhou.chow <124861647+tcs2340445@users.noreply.github.com>
2023-09-09 19:21:19 +0900

    8080

--- node-hello-world/app.js
+++ node-hello-world/app.js
@@ -26,3 +26,37 @@ app.post("/post", function (req, res) {
   res.send("lb neg post test ok --" + req.body +"api key -->" + apiKey);
 });
 
+app.get('/v1/abc/get', (req, res) => {
+  console.log("lb neg get /v1/abc/get path");
+  res.send('Hello path/ -->>/v1/abc/get !!!');
+});
+
+app.post("/v1/abc/post", function (req, res) {
+  console.log("lb neg -->> /v1/abc/post test!!!");
+  const apiKey = req.headers['x-api-key'];
+  console.log("api key -->" + apiKey);
+  res.send("lb neg post test ok --" + req.body +"| api key -->" + apiKey);
+});
+
+app.post("/v1/abc/product/:paramValue", function (req, res) {
+  console.log("lb neg -->> /v1/abc/product/:paramValue test!!!");
+  const apiKey = req.headers['x-api-key'];
+  console.log("api key -->" + apiKey);
+
+  // req.params を使用してパスパラメータの値を取得します
+  const paramValue = req.params.paramValue;
+
+  res.send("lb neg post test ok -- Param Value: " + paramValue + " | API Key: " + apiKey);
+});
+
+// 動的パスパラメータを使用して、{xxxxxxx} の値を取得します
+app.get("/v1/abc/users/:paramValue", function (req, res) {
+  console.log("lb neg -->> /v1/abc/users/:paramValue test!!!");
+  const apiKey = req.headers['x-api-key'];
+  console.log("api key -->" + apiKey);
+
+  // req.params を使用してパスパラメータの値を取得します
+  const paramValue = req.params.paramValue;
+
+  res.send("lb neg get test ok -- Param Value: " + paramValue + " | API Key: " + apiKey);
+});
